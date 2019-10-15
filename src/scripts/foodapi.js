22705457c996f40e227b4fdbf0f7e494b93a6be6
Ryan Bishop <rdbishop06@gmail.com>
2019-10-15 14:57:26 -0500

    Refactor fetch call and create external getValidProductFields function

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,55 +1,72 @@
 // console.log('Git it!');
 
 const foodFactory = (food) => {
-    return `<div class="food">
+	return `<div class="food">
         <h2>${food.name}</h2>
         <p>Ethnicity: ${food.ethnicity}</p>
         <p>Category: ${food.category}</p>
         <p class="ingredient-list">Ingredients:</br>${food.ingredients}</p>
         <p>Country of origin: ${food.countryOrigin}</p>
-        </div>`
-}
+        <p>Calories/serving: ${food.caloriesServing}</p>
+        <p>Fat/serving: ${food.fatServing}</p>
+        <p>Sugar/serving: ${food.sugarServing}</p>
+        </div>`;
+};
 
-let foodList = "";
+let foodList = '';
 
 const addFoodToDom = (foodEl) => {
-    foodList += foodEl;
-    // document.querySelector(".foodList").appendChild(foodEl);
-    document.querySelector(".foodList").innerHTML = foodList;
-}
+	foodList += foodEl;
+	document.querySelector('.foodList').innerHTML = foodList;
+};
 
-// fetch("http://localhost:8088/food")
-// .then(foods => foods.json())
-// .then(parsedFoods => {
-//     parsedFoods.forEach(food => {
-//         const foodAsHTML = foodFactory(food);
-//         addFoodToDom(foodAsHTML);
-//     })
-// })
+const getValidProductFields = (productInfo, food) => {
+	if (productInfo.product.ingredients_text) {
+		food.ingredients = productInfo.product.ingredients_text;
+	} else {
+		food.ingredients = 'No ingredients listed';
+    }
+    if (productInfo.product.countries){
+        food.countryOrigin = productInfo.product.countries;
+    }
+    if (productInfo.product.nutriments.energy_serving) {
+        food.caloriesServing = `${productInfo.product.nutriments.energy_serving} 
+        ${productInfo.product.nutriments.energy_unit}`;
+    }
+    else {
+        food.caloriesServing = 'Not listed'
+    }
+    if (productInfo.product.nutriments.fat_serving) {
+	    food.fatServing = `${productInfo.product.nutriments.fat_serving} 
+        ${productInfo.product.nutriments.fat_unit}`;
+    }
+    else {
+        food.fatServing = 'Not listed';
+    }
+    if (productInfo.product.nutriments.sugars_serving){
+	    food.sugarServing = `${productInfo.product.nutriments.sugars_serving} 
+        ${productInfo.product.nutriments.sugars_unit}`
+    }
+    else {
+        food.sugarServing = 'Not listed';
+    }
+};
 
-fetch("http://localhost:8088/food")
-    .then(response => response.json())
-    .then(myParsedFoods => {
-        myParsedFoods.forEach(food => {
-            console.log(food) // Should have a `barcode` property
+fetch('http://localhost:8088/food').then((response) => response.json()).then((myParsedFoods) => {
+	myParsedFoods.forEach((food) => {
+		console.log(food); // Should have a `barcode` property
 
-            // Now fetch the food from the Food API
-            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
-                .then(response => response.json())
-                .then(productInfo => {
-                    if (productInfo.product.ingredients_text) {
-                      food.ingredients = productInfo.product.ingredients_text
-                    } else {
-                      food.ingredients = "no ingredients listed"
-                    }
-                    food.countryOrigin = productInfo.product.countries[0]
-                    food.caloriesServing = productInfo.product.nutriments.energy_serving
+		// Now fetch the food from the Food API
+		fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+			.then((response) => response.json())
+			.then((productInfo) => {
+				getValidProductFields(productInfo, food); // refactored into external function
 
-                    // Produce HTML representation
-                    const foodAsHTML = foodFactory(food)
+				// Produce HTML representation
+				const foodAsHTML = foodFactory(food);
 
-                    // Add representaiton to DOM
-                    addFoodToDom(foodAsHTML)
-                })
-        })
-    })
+				// Add representaiton to DOM
+				addFoodToDom(foodAsHTML);
+			});
+	});
+});
